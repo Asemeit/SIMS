a7393f6ef8ec44693b1d111b4ac5db04f279dc8f
Asemeit <155088617+Asemeit@users.noreply.github.com>
2026-03-31 18:50:59 +0300

    Final updates before Vercel deploy

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.asemeit.sims',
+  appName: 'SIMS',
+  webDir: 'dist'
+};
+
+export default config;
