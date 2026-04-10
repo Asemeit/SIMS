3de245740db15caf075d512ec92d7a02912eb7cd
Asemeit <155088617+Asemeit@users.noreply.github.com>
2026-04-10 11:20:09 +0300

    chore: force immediate service worker updates for normal browsers

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { ThemeProvider } from './components/layout/ThemeProvider.tsx';
-
 import { SettingsProvider } from './context/SettingsContext.tsx';
+import { registerSW } from 'virtual:pwa-register';
+
+// Automatically update the Service Worker when a new version is available
+registerSW({ immediate: true });
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
